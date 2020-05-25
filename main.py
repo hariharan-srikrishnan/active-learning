d6441e17f19942723cc4fc18d0b34752cfb6f443
Hariharan <hariharan.srikrishnan123@gmail.com>
2020-05-25 11:14:40 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import os
+import sys
+import numpy as np
+from sklearn.preprocessing import normalize
+from sklearn.preprocessing import StandardScaler
